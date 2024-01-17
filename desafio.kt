c3a8093a4b77941f154d1f1b4a3eb40d97e8e8c5
Wédson Gomes <40505932+wedgomes@users.noreply.github.com>
2024-01-17 00:41:34 -0300

    Ajuste e implementação do metodo matricular e testes com objetos

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,45 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, var formacao: Formacao)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+	
+    var conteudos1 = mutableListOf<ConteudoEducacional>()
+    conteudos1.add(ConteudoEducacional("POO", 180))
+    conteudos1.add(ConteudoEducacional("AED", 90))
+    
+    
+    var conteudos2 = mutableListOf<ConteudoEducacional>()
+    conteudos2.add(ConteudoEducacional("Desenvolvimento com Python", 360))
+    
+    
+    
+    var formacao1 = Formacao("SI", Nivel.INTERMEDIARIO, conteudos1)
+    var usuario1 = Usuario("João", formacao1)
+    
+    
+    var formacao2 = Formacao("Engenharia da Computação", Nivel.DIFICIL, conteudos2)
+    var usuario2 = Usuario("Wanderley", formacao2)
+    
+    formacao1.matricular(usuario1)
+    formacao2.matricular(usuario2)
+    
+    println("Matriculados: SI "+formacao1.inscritos)
+    println("Matriculados: ENG COMP. "+formacao2.inscritos)
+    
+    println("Nome: "+usuario1.nome)
+    println("Formação: "+usuario1.formacao.nome)
+    println("Nivel: "+usuario1.formacao.nivel)
+	
 }
